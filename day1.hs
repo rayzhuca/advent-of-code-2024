45038b358d3288bf37f5725057265b4d60385476
rayzhuca <raybipse@gmail.com>
2024-11-30 23:26:35 -0800

    Formatted day1.hs

--- day1.hs
+++ day1.hs
@@ -1,33 +1,33 @@
-import System.IO
 import Data.List
+import System.IO
 
 totalDist :: [Int] -> [Int] -> Int
 totalDist a b = sum (zipWith (curry (abs . uncurry (-))) a b)
 
 score :: [Int] -> [Int] -> Int
-score a b = sum (map (\x -> x * length (filter (==x) b)) a)
+score a b = sum (map (\x -> x * length (filter (== x) b)) a)
 
 split :: [Int] -> ([Int], [Int])
 split [] = ([], [])
-split (a:b:xs) = (a : as, b : bs)
-  where (as, bs) = split xs
+split (a : b : xs) = (a : as, b : bs)
+  where
+    (as, bs) = split xs
 
 parseIn :: IO ([Int], [Int])
 parseIn = do
-    content <- readFile "in.txt"
-    let numbers = map read (words content) :: [Int]
-    return (split numbers)
+  content <- readFile "in.txt"
+  let numbers = map read (words content) :: [Int]
+  return (split numbers)
 
 partOne :: IO ()
 partOne = do
-    (a, b) <- parseIn
-    let a' = sort a
-    let b' = sort b
-    let result = totalDist a' b'
-    print result
+  (a, b) <- parseIn
+  let a' = sort a
+  let b' = sort b
+  let result = totalDist a' b'
+  print result
 
 partTwo :: IO ()
 partTwo = do
-    (a, b) <- parseIn
-    print (score a b)
-
+  (a, b) <- parseIn
+  print (score a b)

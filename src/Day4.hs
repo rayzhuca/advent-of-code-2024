73bbf9d05abc9c6ddf5f104392affdeb727c8556
rayzhuca <raybipse@gmail.com>
2024-12-05 18:03:08 -0800

    Day 4 Part 2

--- src/Day4.hs
+++ src/Day4.hs
@@ -1,29 +1,39 @@
 import Data.Sequence (Seq, (!?))
 import qualified Data.Sequence as Seq
 
-countXmasAt :: Seq (Seq Char) -> Int -> Int -> Int
-countXmasAt a i j = foldr (\l acc -> acc + (if l == map Just "XMAS" then 1 else 0)) 0 directions
-  where
-    walk = zipWith (\p q -> (a !? (i + p)) >>= (!? (j + q)))
-    forward = [0 .. 3]
-    backward = reverse [-3 .. 0]
-    stay = repeat 0
-    directions =
-      [ walk stay forward,
-        walk backward forward,
-        walk backward stay,
-        walk backward backward,
-        walk stay backward,
-        walk forward backward,
-        walk forward stay,
-        walk forward forward
-      ]
+boolToInt :: Bool -> Int
+boolToInt b = if b then 1 else 0
 
 cartProd :: [a] -> [b] -> [(a, b)]
 cartProd xs ys = [(x, y) | x <- xs, y <- ys]
 
-countXmas :: Seq (Seq Char) -> Int
-countXmas a = sum $ map (uncurry $ countXmasAt a) (cartProd [0 .. n - 1] [0 .. m - 1])
+walk :: Seq (Seq Char) -> Int -> Int -> [Int] -> [Int] -> [Maybe Char]
+walk a i j = zipWith (\p q -> (a !? (i + p)) >>= (!? (j + q)))
+
+countXmasAt :: Seq (Seq Char) -> Int -> Int -> Int
+countXmasAt a i j = foldr (\l acc -> acc + boolToInt (l == map Just "XMAS")) 0 directions
+  where
+    forward = [0 .. 3]
+    backward = reverse [-3 .. 0]
+    stay = repeat 0
+    directions =
+      [ walk a i j stay forward,
+        walk a i j backward forward,
+        walk a i j backward stay,
+        walk a i j backward backward,
+        walk a i j stay backward,
+        walk a i j forward backward,
+        walk a i j forward stay,
+        walk a i j forward forward
+      ]
+
+countXmasAt' :: Seq (Seq Char) -> Int -> Int -> Int
+countXmasAt' a i j = boolToInt $ mas 1 1 && mas 1 (-1)
+  where
+    mas x y = let l = walk a i j [x, 0, -x] [y, 0, -y] in l == map Just "MAS" || l == map Just "SAM"
+
+countXmas :: (Seq (Seq Char) -> Int -> Int -> Int) -> Seq (Seq Char) -> Int
+countXmas f a = sum $ map (uncurry $ f a) (cartProd [0 .. n - 1] [0 .. m - 1])
   where
     n = length a
     m = length (Seq.index a 0)
@@ -38,4 +48,10 @@ partOne :: IO ()
 partOne =
   do
     a <- parseIn
-    print $ countXmas a
+    print $ countXmas countXmasAt a
+
+partTwo :: IO ()
+partTwo =
+  do
+    a <- parseIn
+    print $ countXmas countXmasAt' a
